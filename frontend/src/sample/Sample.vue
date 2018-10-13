307a44410db8854d503457824b2385df3c8db1ca
pandabearcoder <vlouie00@gmail.com>
2018-10-13 06:07:14 +0000

    Configure webpack loader and js reverse lib

---
+++ frontend/src/sample/Sample.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="row">
+  <div class="col">
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: [],
+  components: {
+  },
+  data () {
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ frontend/src/sample/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+import Sample from './Sample.vue'
+
+new Vue({
+  el: '#app-sample',
+  render: h => h(Sample)
+})
